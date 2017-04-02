db04d4a77785fc4f610002e73eef99bffcf17851
inlighter <inlighters@gmail.com>
2017-04-02 13:59:52 +0300

    Update Portfolio

--- Portfolio/RQM/js/main.js
+++ Portfolio/RQM/js/main.js
@@ -1,9 +1,15 @@
-$(function() {
   var currentQuote = "";
-  var currentAuthor = "";
+  var currentAuthor = "Неизвестный автор";
   var currentLink = "";
+  var id = 0;
+  function getRandomNumber(min, max){
+    return Math.floor(Math.random()*(Math.abs(max - min) + 1) + Math.min(max, min));
+  }
+
   function getNextQuote() {
-    $.getJSON("http://www.quotzzy.co/api/quote/?lang=ru", function(data) {
+    var color = getRandomNumber(0, 360);
+    $(".container").css("background", "hsl(" + color + ", 100%, 90%)");
+    $.getJSON("https://crossorigin.me/http://www.quotzzy.co/api/quote/?lang=ru", function(data) {
       console.log(data);
       try {
       currentQuote = data.text;
@@ -14,20 +20,22 @@ $(function() {
       }
 
       var count = 0;
+      var currentID = id;
 
       if (!currentQuote) {
         getNextQuote();
       } else {
-
-      function nextLetter() {
-        $("blockquote").append(currentQuote.charAt(count));
+      function nextLetter(id) {
+        $('blockquote[data-id=' + id + ']').append(currentQuote.charAt(count));
         if (count < currentQuote.length) {
           count += 1;
-          setTimeout(nextLetter, 35);
+
+          setTimeout(function(){
+            nextLetter(id);}, 35);
 
         }
       }
-      nextLetter();
+      nextLetter(currentID);
       $("footer cite a").text("— " + currentAuthor);
       $("footer cite a").attr("href", currentLink);
       $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
@@ -36,17 +44,24 @@ $(function() {
     $("blockquote").text("Произошла ошибка. Попробуйте еще раз.");
   });
   }
+
+
+$(function() {
   if (!$("blockquote").text()) {
-    $(".quote").delay(200).fadeTo(800, 1);
+    $(".quote").delay(350).fadeTo(650, 1);
     getNextQuote();
   }
 
-  $("blockquote").click(function() {
-    $("blockquote").text("");
-    $("footer cite a").text("");
-    $(".quote").fadeOut('fast');
+  $(".quote").click(function() {
+
+    $('blockquote[data-id=' + id + ']').remove();
+    id += 1;
+    $('<blockquote title="Click to generate new quote"></blockquote>').insertBefore("footer");
+    $("blockquote").attr("data-id", id.toString());
+    $("footer cite a").empty();
+    $(".quote").fadeOut(10);
     getNextQuote();
-    $(".quote").delay(200).fadeIn(500);
+    $(".quote").delay(350).fadeIn(650);
   });
 
   $('button').on('click', function() {
