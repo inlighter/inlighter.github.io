552b178b49dc03b05bbec5dda0a6601f89d584f9
inlighter <inlighters@gmail.com>
2017-04-25 15:12:33 +0300

    Fix

--- TODO/main.ts
+++
@@ -1,9 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-import { enableProdMode } from '@angular/core';
-// Enable production mode unless running locally
-if (!/localhost/.test(document.location.host)) {
-  enableProdMode();
-}
-platformBrowserDynamic().bootstrapModule(AppModule);
-
